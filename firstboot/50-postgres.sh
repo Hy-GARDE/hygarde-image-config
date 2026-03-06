55febb1f86d6d8128997ebab9cbe5f2e81bf1436
Louis-Baptiste Sobolewski <lb.sobolewski@iot.bzh>
2026-03-06 16:57:26 +0100

    Fix PostgreSQL firstboot script

--- firstboot/50-postgres.sh
+++ firstboot/50-postgres.sh
@@ -8,16 +8,21 @@
 set -e
 
 # create Postgres data dir
-mkdir /data/pgsql
-chown postgres:postgres /data/pgsql
-chmod 700 /data/pgsql
+echo "firstboot PostgreSQL: create data dir"
+mkdir -p /data/pgsql/data
+chown --recursive postgres:postgres /data/pgsql
+chmod --recursive 700 /data/pgsql
 
 # init and tune database
+echo "firstboot PostgreSQL: init and tune cluster"
 /usr/sbin/postgresql-new-systemd-unit --unit postgresql@hygarde --datadir=/data/pgsql/data
 postgresql-setup --initdb --unit postgresql@hygarde --port 5432
-timescaledb-tune -yes -/data/pgsql/data/postgresql.conf
+timescaledb-tune -yes -conf-path /data/pgsql/data/postgresql.conf
 
 # apply changes
+echo "firstboot PostgreSQL: apply changes and remove tools"
 systemctl restart postgresql@hygarde
 # equivalent of `dnf remove timescaledb-tune`
-rpm -vv -e timescaledb-tune keyutils-libs-devel krb5-devel libcom_err-devel libicu-devel libkadm5 libpkgconf libselinux-devel libsepol-devel libverto-devel openssl-devel pcre2-devel pcre2-utf16 pcre2-utf32 pkgconf pkgconf-m4 pkgconf-pkg-config postgresql-private-devel postgresql-server-devel
+rpm -v -e timescaledb-tune keyutils-libs-devel krb5-devel libcom_err-devel libicu-devel libkadm5 libselinux-devel libsepol-devel libverto-devel openssl-devel pcre2-devel pcre2-utf16 pcre2-utf32 postgresql-private-devel postgresql-server-devel
+
+echo "firstboot PostgreSQL: init done"
